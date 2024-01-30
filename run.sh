641500c64e00f5b968b0981df9921974c6bfb7db
binaybasnetoct13@gmail.com <binaybasnetoct13@gmail.com>
2024-01-30 14:41:29 +0545

    drone test

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DRONE_GITHUB_CLIENT_ID=4142ebe1a0eac28cb6fc
-export DRONE_GITHUB_CLIENT_SECRET=d888f06d81fb0ddb21917ab2da291e6ffde59154
+export DRONE_GITHUB_CLIENT_ID=4a0062c1de77419efbe0
+export DRONE_GITHUB_CLIENT_SECRET=0d3aeaf589398e84dda6588f6058077bfbd3b42b
 export DRONE_GITHUB_ADMIN=Binay-Basnet
-export DRONE_SERVER_HOST=797f-110-44-116-141.ngrok-free.app
+export DRONE_SERVER_HOST=f97a-110-44-116-141.ngrok-free.app
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
