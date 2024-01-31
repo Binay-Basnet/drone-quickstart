7901dbd27939621e36940368566a377e4c4f0481
binaybasnetoct13@gmail.com <binaybasnetoct13@gmail.com>
2024-01-31 10:33:15 +0545

    remote vm setup

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DRONE_GITHUB_CLIENT_ID=3887ca17456972aee2f5
-export DRONE_GITHUB_CLIENT_SECRET=a2d0ee7ed3dc032acef50c60de6f0657f2fc1322
+export DRONE_GITHUB_CLIENT_ID=4745343fc2791cbfc53e
+export DRONE_GITHUB_CLIENT_SECRET=905009612971d1204d00d3431f28ebf76c3ba981
 export DRONE_GITHUB_ADMIN=Binay-Basnet
-export DRONE_SERVER_HOST=f807-2404-7c00-44-cd35-4da3-d13c-8844-fe88.ngrok-free.app
+export DRONE_SERVER_HOST=10aa-110-44-116-141.ngrok-free.app
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
