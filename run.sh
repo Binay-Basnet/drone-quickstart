57d4314a9d5cc638f1331a29e7fd0d78a8f5202d
binaybasnetoct13@gmail.com <binaybasnetoct13@gmail.com>
2024-02-22 11:33:59 +0545

    test: pipeline ssh test to tigg-uat

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-export DRONE_GITHUB_CLIENT_ID=bfb648af20a4d34e57f7
-export DRONE_GITHUB_CLIENT_SECRET=88e02a0f54f9269aa8f6d4532122839534b78eea
+export DRONE_GITHUB_CLIENT_ID=a5916193ec66f1f21e23
+export DRONE_GITHUB_CLIENT_SECRET=52bd6be239ac02811d8b3f630b338fbf399d7867
 export DRONE_GITHUB_ADMIN=Binay-Basnet
-export DRONE_SERVER_HOST=16f7-110-44-116-141.ngrok-free.app
+export DRONE_SERVER_HOST=5273-110-44-116-141.ngrok-free.app
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
 export DRONE_USER_CREATE="username:${DRONE_GITHUB_ADMIN},machine:false,admin:true,token:${DRONE_RPC_SECRET}"
