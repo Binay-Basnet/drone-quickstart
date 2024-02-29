143c1925aca052fdac8ce299c5aab12a86ca92dd
Binay-Basnet <binaybasnetoct13@gmail.com>
2024-02-29 11:52:32 +0545

    test(cicd) different branch test

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-export DRONE_GITHUB_CLIENT_ID=6b4fd11b57ee4e3b9f24
-export DRONE_GITHUB_CLIENT_SECRET=6522d1f017c5e28eb68851c6b31a4e54259e609c
+export DRONE_GITHUB_CLIENT_ID=77baaa96996e1b7542b6
+export DRONE_GITHUB_CLIENT_SECRET=6c98ab353711b9c2a4cdaea28be0701ab16e4ca5
 export DRONE_GITHUB_ADMIN=Binay-Basnet
-export DRONE_SERVER_HOST=2ff9-2404-7c00-43-5296-569a-4b63-51e3-5ee7.ngrok-free.app
+export DRONE_SERVER_HOST=9d1c-110-44-116-141.ngrok-free.app
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
 export DRONE_USER_CREATE="username:${DRONE_GITHUB_ADMIN},machine:false,admin:true,token:${DRONE_RPC_SECRET}"
 docker compose up -d
